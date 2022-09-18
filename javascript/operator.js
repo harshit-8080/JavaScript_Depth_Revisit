8ae2592e01af637d7c548d6e5bb3cf92d6a3a881
harshit71 <harshit.raj@itential.com>
2022-09-18 15:45:22 +0530

    adding operator.js

---
+++ javascript/operator.js
@@ -0,0 +1,13 @@
+console.log(10 + 2); //addition
+
+console.log(10 - 2); // 8
+
+console.log(10 * 2); //20
+
+console.log(10 / 3); //5
+
+console.log(10 % 2); //0
+
+console.log(Math.floor(10/3));
+
+console.log(parseInt(10/3));
