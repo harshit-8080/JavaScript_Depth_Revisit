bdee48a0c56840082ff3ccc3864ce2242bb23766
harshit71 <harshit.raj@itential.com>
2022-09-18 15:51:17 +0530

    adding operator.js 2

---
+++ javascript/logicalOperator.js
@@ -0,0 +1,15 @@
+/**
+ 
+and - &&
+
+or - ||
+
+not - !
+
+ */
+
+console.log((10 > 5) && (7<6));
+
+console.log((10 > 5) || (7<6));
+
+console.log(!true);
--- javascript/operator.js
+++ javascript/operator.js
@@ -11,3 +11,7 @@ console.log(10 % 2); //0
 console.log(Math.floor(10/3));
 
 console.log(parseInt(10/3));
+
+let number = 10;
+
+console.log(number);
