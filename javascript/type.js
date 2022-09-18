6c72d39451bd9189e44600b90bf41768ff40af23
harshit71 <harshit.raj@itential.com>
2022-09-18 15:13:55 +0530

    adding type and typeof code

---
+++ javascript/type.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+console.log(123);
+console.log(3.456);
+console.log(0);
+console.log(-0+1);
+
+console.log(false);
+console.log(true);
+
+console.log('single quotes string');
+console.log(`this is a backtick string`);
+
+console.log(null);
+console.log(undefined);
+
+console.log({
+    "name":"harshit",
+    "age":123
+});
+
+console.log(Symbol('hello'));
+
+console.log(true, null, 13, "harshit");
+
+process.stdout.write("harshit");
+process.stdout.write("world");
---
+++ javascript/typeof.js
@@ -0,0 +1,16 @@
+console.log(typeof(12));
+
+console.log(typeof 100.23);
+
+console.log(typeof "hello world");
+
+console.log(typeof true);
+
+console.log(typeof `backtick`);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof Symbol('hello'));
+console.log(typeof NaN);
