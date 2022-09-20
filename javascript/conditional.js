b00462e4a63b9cb6bb5af5f360cb3b134924b48d
harshit71 <harshit.raj@itential.com>
2022-09-20 21:07:14 +0530

    adding array and conditional

---
+++ javascript/conditional.js
@@ -0,0 +1,31 @@
+if(10<2){
+    console.log("if ");
+}
+else{
+    console.log("else ");
+}
+
+let s = "harshit";
+
+if(s){
+    console.log("if");
+}
+else{
+    console.log("else");
+}
+
+let d = 100;
+
+if(d){
+    console.log("if");
+}
+else{
+    console.log("else");
+}
+
+if(!d){
+    console.log("if");
+}
+else{
+    console.log("else");
+}
