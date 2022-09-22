1f97786b256bb180ce7dd1ffd61bccea345e5db2
harshit71 <harshit.raj@itential.com>
2022-09-22 22:15:16 +0530

    adding corecion.js

---
+++ javascript/corecion.js
@@ -0,0 +1,38 @@
+// console.log("1" - 1);
+
+// console.log(2 - "1");
+
+// console.log("10" - "5");
+
+// console.log("harshit" - "harshit");
+
+// // -----------------------
+// console.log("1" + 1);
+
+// console.log(2 + "1");
+
+// console.log("10" + "5");
+
+// console.log("10" - "5");
+// console.log("10" + "5");
+// console.log("10" * "5");
+// console.log("10" / "5");
+// console.log("10" % "5");
+
+// console.log("1" - 1);
+// console.log("1" + 1);
+// console.log("1" * 1);
+// console.log("1" / 1);
+// console.log("1" % 1);
+
+// console.log("1.34" - 1);
+
+
+// console.log(0/0);
+
+console.log(undefined - 10);
+
+console.log(null - 10);
+console.log(null - "10");
+
+console.log(undefined - "10");
