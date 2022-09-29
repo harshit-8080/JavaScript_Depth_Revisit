4f2a6af213871e7e3b77c280c6fe624844e03443
harshit71 <harshit.raj@itential.com>
2022-09-29 20:47:16 +0530

    adding nan code

--- javascript/equality.js
+++ javascript/equality.js
@@ -18,3 +18,15 @@ console.log(true == 1);
 
 console.log(10 == {a:100});
 console.log(10 == { valueOf() { return 10}});
+
+console.log(100 == NaN);
+console.log(100 == undefined);
+console.log(100 == null);
+
+a = 5;
+if(a == true){
+    console.log("if");
+}
+else{
+    console.log("else");
+}
---
+++ javascript/nan.js
@@ -0,0 +1,18 @@
+console.log(typeof NaN);
+
+// it doesn't actucally mean not a number, but rather it guess
+// notion of an invalid number;
+
+// we can use NaN, when we want to return invalid number.
+// example - returning index as -1 in javascript.
+
+
+console.log(NaN == 100);
+console.log(100 == NaN);
+console.log(NaN == NaN);
+// it's only primitive which doesn't equal to itself
+
+
+
+console.log(typeof NaN == typeof NaN);
+
