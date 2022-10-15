ac7204a8b6d7e2903748946de58b57fbfa2f8e2f
harshit71 <harshit.raj@itential.com>
2022-10-15 14:13:32 +0530

    adding notes for comma and small brackets

--- javascript/testComma.js
+++ javascript/testComma.js
@@ -10,3 +10,18 @@ console.log(c);
 d = (1+2,3,4,80+10)
 
 console.log(d);
+
+
+/**
+ 
+ Point 1:- if we have value separated as value 
+ like var a = 10,20
+ then a = leftSide value i:e 10
+
+
+
+ 
+ Point 2:- if we have value in small brackets 
+ like var a = (10,20)
+ then a = rightSide value i:e 20
+ */
