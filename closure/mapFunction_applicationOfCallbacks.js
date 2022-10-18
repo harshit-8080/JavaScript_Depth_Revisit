27b5731af0d8bd596c285940480a4adf403a6d29
harshit71 <harshit.raj@itential.com>
2022-10-18 20:52:38 +0530

    map function - it's application of callbacks

---
+++ closure/mapFunction_applicationOfCallbacks.js
@@ -0,0 +1,25 @@
+let arr = [10,20,30,40,50];
+
+let x = arr.map(function process(v,i){
+
+    return v**3;
+    
+    /**
+     *  v -> element of array (one by one)
+     *  i -> index of array
+     * 
+     */
+  
+
+})
+
+let y = arr.map((v,i) => {return v**2});
+
+
+
+console.log(arr);
+console.log(x);
+
+
+console.log(arr);
+console.log(y);
