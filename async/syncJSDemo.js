88fb3941af4cb9a144d923e33d35573b5ff5077a
harshit71 <harshit.raj@itential.com>
2022-10-22 14:25:53 +0530

    js sync code execution

---
+++ async/syncJSDemo.js
@@ -0,0 +1,13 @@
+console.log("starting");
+
+for(let i =0; i<1000000000; i++){
+    // same task
+}
+
+console.log("task done");
+
+console.log("end");
+
+
+// js do code execution in sync manner
+// and do js sync code exec. things only opeartions which it knows  
