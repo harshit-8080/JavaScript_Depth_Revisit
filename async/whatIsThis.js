6d7f271629f7cd49f61dea593f0c94d32e7055ce
harshit71 <harshit.raj@itential.com>
2022-10-22 14:45:03 +0530

    adding runTime notes

---
+++ async/whatIsThis.js
@@ -0,0 +1,32 @@
+console.log("start");
+
+setTimeout(() => {
+    console.log("exec code");
+}, 2000);
+
+console.log("end");
+
+/**
+
+
+
+    JS - Runtime (browser runtime and Node js runtime developed by ryan dahl)
+        chrome and node both uses - v8 engine
+
+        browser's properties --------->
+            dom apis
+            setTimeOut
+            setInterval
+            console.log
+
+        node's properties  ------------->
+            setTimeOut
+            setInterval
+            console.log   
+            
+            file system
+            process
+
+
+
+ */
