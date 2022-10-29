485b5d91dd738d2a2064bd4537a51c50c3f36ee1
harshit71 <harshit.raj@itential.com>
2022-10-29 15:14:03 +0530

    dry run for promise xamples

---
+++ promises/microTask_queue.js
@@ -0,0 +1,23 @@
+console.log("started");
+
+setTimeout(() => {
+    console.log("timer 1 ");
+}, 0);
+
+
+let x = Promise.resolve("harshit");
+x.then((value)=>{
+    console.log(value);
+})
+
+
+// callback inside .then will go to microtask queue not event queue.
+// when this is callback will go to microtask queue ?? --> They will go
+// priority of microtask queue is higher than event queue.
+
+
+setTimeout(() => {
+    console.log("timer  2");
+}, 0);
+
+console.log("end of the file");
