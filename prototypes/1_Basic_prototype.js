b7d14dace79655ad7cd400dc943f4b3fca6aee11
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-27 01:09:46 +0530

    basics of prototypes in js

---
+++ prototypes/1_Basic_prototype.js
@@ -0,0 +1,32 @@
+function Product(name,price) {
+    
+    this.name = name;
+    this.price = price;
+}
+
+const p = new Product("iphone", 100000);
+
+
+
+Product.prototype.display = function display() {
+    
+    console.log(`Name of Product = ${this.name} and Price of Product = ${this.price}`);
+}
+Product.prototype.fun1 = function display() {
+    
+    console.log("this is function 2 ");
+}
+Product.prototype.fun2 = function display() {
+    
+    console.log("this is function 2 ");
+}
+Product.prototype.fun3 = function display() {
+    
+    console.log("this is function 4 ");
+}
+
+
+p.display();
+p.fun1();
+p.fun2();
+p.fun3();
